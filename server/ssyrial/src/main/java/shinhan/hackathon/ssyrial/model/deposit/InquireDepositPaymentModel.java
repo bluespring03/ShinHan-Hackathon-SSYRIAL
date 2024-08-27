60ff1fe8853bff95fcac63052a355dd541229832
khy0728kr <55488481+khy0728kr@users.noreply.github.com>
2024-08-27 12:03:28 +0900

    Feat(server): InquireDepositPaymentModel(예금 납입 상세 조회) 구현
    Feat(server): InquireDepositPaymentModel(예금 납입 상세 조회) 구현

--- server/ssyrial/src/main/java/shinhan/hackathon/ssyrial/model/deposit/InquireDepositPaymentModel.java
+++ server/ssyrial/src/main/java/shinhan/hackathon/ssyrial/model/deposit/InquireDepositPaymentModel.java
@@ -16,3 +16,72 @@ paymentDate - 납입일자 - String - 길이8 - 필수Y
 paymentTime - 납입시각 - String - 길이6 - 필수Y
 paymentBalance - 납입금액 - Long - 길이X - 필수Y
 */
+
+package shinhan.hackathon.ssyrial.model.deposit;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+/**
+ * InquireDepositPaymentModel 클래스는 예금 납입 내역 상세 조회 요청 및 응답 데이터를 담는 모델 클래스입니다.
+ */
+public class InquireDepositPaymentModel {
+
+    /**
+     * 예금 납입 내역 상세 조회 요청 데이터를 담는 내부 클래스입니다.
+     */
+    @Getter
+    @Setter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    @Builder
+    public static class Request {
+        @JsonProperty("Header")
+        private CommonHeaderModel.Request Header;
+
+        @JsonProperty("accountNo")
+        private String accountNo; // 필수: 계좌번호, 길이: 16
+    }
+
+    /**
+     * 예금 납입 내역 상세 조회 응답 데이터를 담는 내부 클래스입니다.
+     */
+    @Getter
+    @Setter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class Response {
+
+        @JsonProperty("Header")
+        private CommonHeaderModel.Response Header;
+
+        @JsonProperty("REC")
+        private DepositPaymentDetail REC; // 예금 납입 내역
+
+        /**
+         * 예금 납입 내역 데이터를 담는 내부 클래스입니다.
+         */
+        @Getter
+        @Setter
+        @NoArgsConstructor
+        @AllArgsConstructor
+        public static class DepositPaymentDetail {
+            @JsonProperty("paymentUniqueNo")
+            private String paymentUniqueNo; // 필수: 납입 고유번호
+
+            @JsonProperty("paymentDate")
+            private String paymentDate; // 필수: 납입일자, 길이: 8
+
+            @JsonProperty("paymentTime")
+            private String paymentTime; // 필수: 납입시각, 길이: 6
+
+            @JsonProperty("paymentBalance")
+            private Long paymentBalance; // 필수: 납입금액
+        }
+    }
+}
