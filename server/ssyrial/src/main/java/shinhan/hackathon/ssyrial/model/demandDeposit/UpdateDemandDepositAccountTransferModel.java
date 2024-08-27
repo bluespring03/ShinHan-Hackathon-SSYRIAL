144b69ed136c8a7747c4b4c0a0e6e86942ca02bb
khy0728kr <55488481+khy0728kr@users.noreply.github.com>
2024-08-27 11:47:00 +0900

    Feat(server): UpdateDemandDepositAccountTransferModel(계좌 이체) 구현
    Feat(server): UpdateDemandDepositAccountTransferModel(계좌 이체) 구현

--- server/ssyrial/src/main/java/shinhan/hackathon/ssyrial/model/demandDeposit/UpdateDemandDepositAccountTransferModel.java
+++ server/ssyrial/src/main/java/shinhan/hackathon/ssyrial/model/demandDeposit/UpdateDemandDepositAccountTransferModel.java
@@ -22,3 +22,90 @@ transactionType - 거래유형 - String - 길이1 - 필수Y - 1, 2 ...
 transactionTypeName - 거래유형명 - String - 길이8 - 필수Y - 입금이체, 출금이체 ...
 transactionAccountNo - 거래 계좌번호 - String - 길이16 - 필수Y - 이체 거래에 대한 계좌번호
 */
+
+package shinhan.hackathon.ssyrial.model.demandDeposit;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+/**
+ * UpdateDemandDepositAccountTransferModel 클래스는 계좌 이체 요청 및 응답 데이터를 담는 모델 클래스입니다.
+ */
+public class UpdateDemandDepositAccountTransferModel {
+
+    /**
+     * 계좌 이체 요청 데이터를 담는 내부 클래스입니다.
+     */
+    @Getter
+    @Setter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    @Builder
+    public static class Request {
+        @JsonProperty("Header")
+        private CommonHeaderModel.Request Header;
+
+        @JsonProperty("depositAccountNo")
+        private String depositAccountNo; // 필수: 입금계좌번호, 길이: 16
+
+        @JsonProperty("transactionBalance")
+        private Long transactionBalance; // 필수: 거래금액
+
+        @JsonProperty("withdrawalAccountNo")
+        private String withdrawalAccountNo; // 필수: 출금계좌번호, 길이: 16
+
+        @JsonProperty("depositTransactionSummary")
+        private String depositTransactionSummary; // 선택: 거래 요약내용(입금계좌), 길이: 255
+
+        @JsonProperty("withdrawalTransactionSummary")
+        private String withdrawalTransactionSummary; // 선택: 거래 요약내용(출금계좌), 길이: 255
+    }
+
+    /**
+     * 계좌 이체 응답 데이터를 담는 내부 클래스입니다.
+     */
+    @Getter
+    @Setter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class Response {
+
+        @JsonProperty("Header")
+        private CommonHeaderModel.Response Header;
+
+        @JsonProperty("REC")
+        private TransactionInfo REC; // 거래목록
+
+        /**
+         * 거래 정보를 담는 내부 클래스입니다.
+         */
+        @Getter
+        @Setter
+        @NoArgsConstructor
+        @AllArgsConstructor
+        public static class TransactionInfo {
+            @JsonProperty("transactionUniqueNo")
+            private Long transactionUniqueNo; // 필수: 거래고유번호
+
+            @JsonProperty("accountNo")
+            private String accountNo; // 필수: 계좌번호, 길이: 16
+
+            @JsonProperty("transactionDate")
+            private String transactionDate; // 필수: 거래일자, 길이: 8, YYYYMMDD
+
+            @JsonProperty("transactionType")
+            private String transactionType; // 필수: 거래유형, 길이: 1
+
+            @JsonProperty("transactionTypeName")
+            private String transactionTypeName; // 필수: 거래유형명, 길이: 8
+
+            @JsonProperty("transactionAccountNo")
+            private String transactionAccountNo; // 필수: 거래 계좌번호, 길이: 16
+        }
+    }
+}
