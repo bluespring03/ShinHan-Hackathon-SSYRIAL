dd060080f43a06ef40efd8631de27e796a7a094f
sooyeon Park <20200918@sungshin.ac.kr>
2024-08-29 17:45:03 +0900

    Update 2.sign_in_password.dart

--- client/lib/sooyeon_front/2.sign_in_password.dart
+++ client/lib/sooyeon_front/2.sign_in_password.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart'; // 서버 연결 시 사용하는 패키지 추가
+import 'dart:async'; // 타이머 기능 추가
+import '3.fail_password_popup.dart'; // Fail Password Popup 연결
 
 void main() {
   runApp(MyApp());
@@ -25,7 +28,9 @@ class SignInScreen extends StatefulWidget {
 
 class _SignInScreenState extends State<SignInScreen> {
   String password = '';
+  String correctPassword = '123456'; // 서버에서 가져온 비밀번호로 대체 가능
 
+  // 숫자를 추가하는 함수
   void addNumber(String number) {
     if (password.length < 6) {
       setState(() {
@@ -34,6 +39,7 @@ class _SignInScreenState extends State<SignInScreen> {
     }
   }
 
+  // 숫자를 지우는 함수
   void removeNumber() {
     if (password.isNotEmpty) {
       setState(() {
@@ -42,9 +48,30 @@ class _SignInScreenState extends State<SignInScreen> {
     }
   }
 
+  // 비밀번호를 초기화하는 함수
   void clearPassword() {
     setState(() {
       password = '';
+    });
+  }
+
+  // 비밀번호 확인 함수
+  void verifyPassword() {
+    if (password == correctPassword) {
+      print('비밀번호 일치');
+      // 비밀번호 일치 시의 로직 추가
+    } else {
+      print('비밀번호 불일치');
+      showFailPopup(); // 비밀번호 불일치 시 팝업 호출
+    }
+  }
+
+  // 비밀번호 불일치 시 Fail Popup을 3초간 보여줌
+  void showFailPopup() {
+    showFailPasswordPopup(context); // 팝업 표시
+    Timer(Duration(seconds: 3), () {
+      Navigator.of(context).pop(); // 3초 후 팝업 닫기
+      clearPassword(); // 비밀번호 초기화
     });
   }
 
@@ -73,27 +100,27 @@ class _SignInScreenState extends State<SignInScreen> {
                       children: List.generate(
                         6,
                             (index) => Container(
-                          width: 40, // 각 박스의 너비를 설정
-                          height: 40, // 각 박스의 높이를 설정
+                          width: 40,
+                          height: 40,
                           margin: EdgeInsets.symmetric(horizontal: 4),
                           decoration: BoxDecoration(
                             border: Border.all(
-                              color: Colors.black, // 테두리 색상 설정
-                              width: 2, // 테두리 두께 설정
+                              color: Colors.black,
+                              width: 2,
                             ),
-                            borderRadius: BorderRadius.circular(8), // 모서리를 둥글게 설정
+                            borderRadius: BorderRadius.circular(8),
                           ),
-                          alignment: Alignment.center, // 텍스트를 가운데 정렬
+                          alignment: Alignment.center,
                           child: Text(
-                            index < password.length ? '*' : '', // 입력된 경우 '*' 표시, 비어 있으면 빈 문자열
+                            index < password.length ? '*' : '',
                             style: TextStyle(
-                              fontSize: 24, // 별표 크기 설정
-                              color: index < password.length ? Colors.black : Colors.grey[300], // 색상 조정
+                              fontSize: 24,
+                              color: index < password.length ? Colors.black : Colors.grey[300],
                             ),
                           ),
                         ),
                       ),
-                    )
+                    ),
                   ],
                 ),
               ),
@@ -113,8 +140,8 @@ class _SignInScreenState extends State<SignInScreen> {
                           child: Text(
                             '${index + 1}',
                             style: TextStyle(
-                              fontWeight: FontWeight.bold, // 볼드체 설정
-                              fontSize: 20, // 폰트 크기 설정
+                              fontWeight: FontWeight.bold,
+                              fontSize: 20,
                               color: Colors.white,
                             ),
                           ),
@@ -126,20 +153,14 @@ class _SignInScreenState extends State<SignInScreen> {
                         child: Text(
                           '재배열',
                           style: TextStyle(
-                            fontWeight: FontWeight.bold, // 볼드체 설정
-                            fontSize: 24, // 폰트 크기 설정
+                            fontWeight: FontWeight.bold,
+                            fontSize: 24,
                           ),
                         ),
-                        onPressed: () {
-                          // 비밀번호 초기화 로직
-                          setState(() {
-                            password = ''; // 비밀번호를 초기화
-                          });
-                          print('비밀번호가 초기화되었습니다.');
-                        },
+                        onPressed: clearPassword,
                         style: ElevatedButton.styleFrom(
-                          backgroundColor: Colors.grey, // 배경색 설정
-                          minimumSize: Size(double.infinity, 60), // 최소 높이 설정
+                          backgroundColor: Colors.grey,
+                          minimumSize: Size(double.infinity, 60),
                         ),
                       ),
                       ElevatedButton(
@@ -149,16 +170,16 @@ class _SignInScreenState extends State<SignInScreen> {
                       ),
                       ElevatedButton(
                         child: Text(
-                          ' 하나\n지우기',
+                          '하나\n지우기',
                           style: TextStyle(
-                            fontWeight: FontWeight.bold, // 볼드체 설정
-                            fontSize: 24, // 폰트 크기 설정
+                            fontWeight: FontWeight.bold,
+                            fontSize: 24,
                           ),
                         ),
                         onPressed: removeNumber,
                         style: ElevatedButton.styleFrom(
-                          backgroundColor: Colors.grey, // 배경색 설정
-                          minimumSize: Size(double.infinity, 60), // 최소 높이 설정
+                          backgroundColor: Colors.grey,
+                          minimumSize: Size(double.infinity, 60),
                         ),
                       ),
                     ],
@@ -167,32 +188,27 @@ class _SignInScreenState extends State<SignInScreen> {
               ),
               Row(
                 children: [
-                  // 왼쪽 '취소' 버튼 (빨간색)
                   Expanded(
                     child: ElevatedButton(
                       child: Text('취소'),
                       onPressed: () {
-                        // 취소 버튼 로직
+                        Navigator.pop(context);
                         print('취소 버튼 클릭됨');
                       },
                       style: ElevatedButton.styleFrom(
                         backgroundColor: Colors.red,
-                        minimumSize: Size(double.infinity, 60), // 최소 높이 설정
+                        minimumSize: Size(double.infinity, 60),
                       ),
                     ),
                   ),
-                  SizedBox(width: 16), // 두 박스 사이의 간격
-                  // 오른쪽 '완료' 버튼 (파란색)
+                  SizedBox(width: 16),
                   Expanded(
                     child: ElevatedButton(
                       child: Text('완료'),
-                      onPressed: () {
-                        // 완료 버튼 로직
-                        print('완료 버튼 클릭됨');
-                      },
+                      onPressed: verifyPassword, // 비밀번호 확인 호출
                       style: ElevatedButton.styleFrom(
                         backgroundColor: Colors.blue,
-                        minimumSize: Size(double.infinity, 60), // 최소 높이 설정
+                        minimumSize: Size(double.infinity, 60),
                       ),
                     ),
                   ),
